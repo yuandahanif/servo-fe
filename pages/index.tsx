8a08e4fbb48a22cbcee5f8da4cc2fa10c475e3f4
yuanda <6232147-yuandahanif@users.noreply.gitlab.com>
2021-06-14 21:15:43 +0700

    add react scanner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,40 @@
-import styles from "../styles/Home.module.css";
-import Layout from "../components/layout";
+import Layout from '../components/layout'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+
+const QrReader = dynamic(() => import('react-qr-reader'), {
+  ssr: false,
+})
 
 export default function Home() {
+  const [result, setResult] = useState('no data')
+
+  const [displayScanner, setDisplayScanner] = useState(false)
+
+  const handleScan = (data: string | null) => {
+    if (data) {
+      setResult(data)
+    }
+  }
+
+  const handleError = (err: {}) => {
+    console.error(err)
+  }
+
   return (
     <Layout>
-      <h1 className={styles.title}>
+      <h1 className="">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-
-      <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/index.js</code>
-      </p>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
+      {displayScanner && (
+        <QrReader
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style='w-40'
+        />
+      )}
+      <p>{result}</p>
     </Layout>
-  );
+  )
 }
